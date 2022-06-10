8bf67a936aa95a9f6ca9c551e8a41d4a6df5f5ac
Ankit Jaiswal <ankit.jaiswal1605@gmail.com>
2022-06-10 22:19:17 +0530

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-Moralis.initialize("NORrQ2YEzHy74mvcqV7KVRJs4LsWF1hzhm175Ejk"); // Application id from moralis.io 
-Moralis.serverURL = "https://7oyfxwjhb9sq.usemoralis.com:2053/server"; //Server url from moralis.io
+Moralis.initialize("7HsV4TcX1Gic7Z6YJvs06ZjMrwW23IvyPrxVUPOw"); // Application id from moralis.io 
+Moralis.serverURL = "https://yaprbh5q0fg7.usemoralis.com:2053/server"; //Server url from moralis.io
 
 init = async () => {
     window.web3 = await Moralis.enableWeb3();
@@ -25,6 +25,120 @@ buy = async () => {
         alert("Please connect wallet");
     }
         
+}
+
+faq1Open = () => {
+    document.getElementById("ad1").style.display = "none";
+    document.getElementById("au1").style.display = "flex";
+    document.getElementById("ans1").style.display = "flex";
+}
+
+faq1Close = () => {
+    document.getElementById("ad1").style.display = "flex";
+    document.getElementById("au1").style.display = "none";
+    document.getElementById("ans1").style.display = "none";
+}
+
+faq2Open = () => {
+    document.getElementById("ad2").style.display = "none";
+    document.getElementById("au2").style.display = "flex";
+    document.getElementById("ans2").style.display = "flex";
+}
+
+faq2Close = () => {
+    document.getElementById("ad2").style.display = "flex";
+    document.getElementById("au2").style.display = "none";
+    document.getElementById("ans2").style.display = "none";
+}
+
+faq3Open = () => {
+    document.getElementById("ad3").style.display = "none";
+    document.getElementById("au3").style.display = "flex";
+    document.getElementById("ans3").style.display = "flex";
+}
+
+faq3Close = () => {
+    document.getElementById("ad3").style.display = "flex";
+    document.getElementById("au3").style.display = "none";
+    document.getElementById("ans3").style.display = "none";
+}
+
+faq4Open = () => {
+    document.getElementById("ad4").style.display = "none";
+    document.getElementById("au4").style.display = "flex";
+    document.getElementById("ans4").style.display = "flex";
+}
+
+faq4Close = () => {
+    document.getElementById("ad4").style.display = "flex";
+    document.getElementById("au4").style.display = "none";
+    document.getElementById("ans4").style.display = "none";
+}
+
+faq5Open = () => {
+    document.getElementById("ad5").style.display = "none";
+    document.getElementById("au5").style.display = "flex";
+    document.getElementById("ans5").style.display = "flex";
+}
+
+faq5Close = () => {
+    document.getElementById("ad5").style.display = "flex";
+    document.getElementById("au5").style.display = "none";
+    document.getElementById("ans5").style.display = "none";
+}
+
+faq6Open = () => {
+    document.getElementById("ad6").style.display = "none";
+    document.getElementById("au6").style.display = "flex";
+    document.getElementById("ans6").style.display = "flex";
+}
+
+faq6Close = () => {
+    document.getElementById("ad6").style.display = "flex";
+    document.getElementById("au6").style.display = "none";
+    document.getElementById("ans6").style.display = "none";
+}
+
+mobileNavbarOpen = () => {
+    document.getElementById("mobile-intro").style.display = "flex";
+    document.getElementById("navbar").style.display = "none";
+    document.getElementsByClassName("video-container")[0].style.display="none";
+    var x = document.getElementsByClassName("wrap");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none';
+    }
+}
+
+mobileNavbarClose = () => {
+    document.getElementById("mobile-intro").style.display = "none";
+    document.getElementById("navbar").style.display = "flex";
+    document.getElementsByClassName("video-container")[0].style.display="flex";
+    var x = document.getElementsByClassName("wrap");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'flex';
+    }
+}
+
+mobileNavbarOpen2 = () => {
+    document.getElementById("mobile-intro2").style.display = "flex";
+    document.getElementById("navbar2").style.display = "none";
+    var x = document.getElementsByClassName("hide");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none';
+    }
+}
+
+mobileNavbarClose2 = () => {
+    document.getElementById("mobile-intro2").style.display = "none";
+    document.getElementById("navbar2").style.display = "flex";
+    var x = document.getElementsByClassName("hide");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'flex';
+    }
 }
 
 const userConnectButton = document.getElementById("btnConnect");
